97a2d5c163657ffdbecf249fd75e4cbde320bf2e
Clifford Tabares <tabarec1@universalleaf.com>
2026-01-14 15:56:16 +0800

    Add angular material.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  protected readonly title = 'client';
+  protected readonly title = 'Skinet';
 }
