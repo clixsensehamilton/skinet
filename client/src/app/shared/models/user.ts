ede4228e64ed7b6cac8552dd7dec2e0ef1c1384c
Clifford Tabares <tabarec1@universalleaf.com>
2026-01-24 16:41:48 +0800

    Add login and register components.

---
+++ client/src/app/shared/models/user.ts
@@ -0,0 +1,15 @@
+export type User = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  address: string;
+};
+
+export type Adress = {
+  line1: string;
+  line2?: string;
+  city: string;
+  state: string;
+  count: string;
+  postalCode: string;
+};
